cd93c7e62e89de0f21045299055e4eb39f4f1c57
eyad-hamdy98 <eyad.hamdy98@gmail.com>
2025-11-05 14:37:29 +0200

    Enhace product card style

---
+++ src/utils/function.ts
@@ -0,0 +1,11 @@
+/**
+ *
+ * @param {string} txt - The input text to be sliced
+ * @param {number} [max=50] - The maximum length before trunation
+ * @returns The sliced text, with an ellipsis (...) appended if truncated
+ */
+
+export function txtSlicer(txt: string, max: number = 50) {
+  if (txt.length >= max) return `${txt.slice(0, max)}...`;
+  return txt;
+}
